34b9687ec64dac95df283732984a53ca2a029a68
drodevilosun <drodevilosun@gmail.com>
2018-04-04 00:09:13 +0700

    Create Investigate.c

---
+++ Investigate.c
@@ -0,0 +1,2 @@
+Weibsite pratice ML with Python: https://pythonprogramming.net/machine-learning-tutorial-python-introduction/
+Refs the answer: https://www.quora.com/Where-can-I-start-learning-how-to-use-TensorFlow

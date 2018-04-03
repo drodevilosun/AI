77fc01bd99775d797e2591ac01d9cc5b65e8d57e
drodevilosun <drodevilosun@gmail.com>
2018-04-04 00:13:46 +0700

    Update Investigate.c

--- Investigate.c
+++ Investigate.c
@@ -1,2 +1,5 @@
 Weibsite pratice ML with Python: https://pythonprogramming.net/machine-learning-tutorial-python-introduction/
-Refs the answer: https://www.quora.com/Where-can-I-start-learning-how-to-use-TensorFlow
+Refs the answer: 
+- https://www.quora.com/Where-can-I-start-learning-how-to-use-TensorFlow
+- https://www.quora.com/How-do-I-learn-Machine-Learning-in-10-days
+- https://www.quora.com/What-is-a-programmers-life-like

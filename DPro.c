f9b1c213a3488954cc3c0551669bd2c1ea2b50ba
drodevilosun <drodevilosun@gmail.com>
2018-04-19 09:07:59 +0700

    Update DPro.c

--- DPro.c
+++ DPro.c
@@ -3,3 +3,6 @@ Refs the answer:
 - https://www.quora.com/Where-can-I-start-learning-how-to-use-TensorFlow
 - https://www.quora.com/How-do-I-learn-Machine-Learning-in-10-days
 - https://www.quora.com/What-is-a-programmers-life-like
+
+CS231 Stanford:
+- https://www.youtube.com/watch?v=vT1JzLTH4G4&list=PL3FW7Lu3i5JvHM8ljYj-zLfQRF3EO8sYv

07e6d9a36da0b6ff4cb289c71108e8555aea243f
drodevilosun <drodevilosun@gmail.com>
2018-08-15 00:02:46 +0700

    Update DPro.c

--- DPro.c
+++ DPro.c
@@ -6,3 +6,6 @@ Refs the answer:
 
 CS231 Stanford:
 - https://www.youtube.com/watch?v=vT1JzLTH4G4&list=PL3FW7Lu3i5JvHM8ljYj-zLfQRF3EO8sYv
+
+Sklearn tree:
+- https://machinelearningcoban.com/2018/01/14/id3/
